76526e959927bfba3e1025a6c82992507390ff27
Anusuya <anuknkp@gmail.com>
2025-06-19 07:45:25 +0530

    Knapsack - code corrections

--- Recursion/src/DynamicProgramming/Knapsack.java
+++ Recursion/src/DynamicProgramming/Knapsack.java
@@ -8,24 +8,21 @@ public class Knapsack {
         int[] itemWeightArray = {1,2,3};
         int maxCapacityOfBag = 4;
         System.out.println( findMaxProfit(maxCapacityOfBag, itemProfitArray, itemWeightArray, maxCapacityOfBag, itemProfitArray.length) );
-
     }
 
     public static int findMaxProfit( int maxCapacityOfBag, int[] itemProfitArray, int[] itemWeightArray, int avlCapacity, int n ) {
         // Base Condition
-        if(n == 0) {
+        if(n == 0 || avlCapacity == 0)  {
             return 0;
         }
-        if( itemWeightArray[n-1] > avlCapacity ) {
-            return 0;
-        }
-
         // Main Logic
+        if( itemWeightArray[n-1] > avlCapacity) {
+            return findMaxProfit(maxCapacityOfBag, itemProfitArray, itemWeightArray, avlCapacity, n-1);
+        }
         // Take
-        int maxProfitByTakingItem = itemProfitArray[n-1] + findMaxProfit(maxCapacityOfBag, itemProfitArray, itemWeightArray, maxCapacityOfBag-itemWeightArray[n-1], n-1);
-
+        int maxProfitByTakingItem = itemProfitArray[n-1] + findMaxProfit(maxCapacityOfBag, itemProfitArray, itemWeightArray, avlCapacity-itemWeightArray[n-1], n-1);
         // Don't Take
-        int maxProfitByNotTakingItem = findMaxProfit(maxCapacityOfBag, itemProfitArray, itemWeightArray, maxCapacityOfBag, n-1);
+        int maxProfitByNotTakingItem = findMaxProfit(maxCapacityOfBag, itemProfitArray, itemWeightArray, avlCapacity, n-1);
 
         System.out.println(n+" "+max(maxProfitByTakingItem, maxProfitByNotTakingItem));
         return max(maxProfitByTakingItem, maxProfitByNotTakingItem);
